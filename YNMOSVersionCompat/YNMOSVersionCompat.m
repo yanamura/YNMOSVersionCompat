10122b9638ab02cc0c63765f0ecee4bec88d6790
Yasuharu Yanamura <yanamura3@gmail.com>
2014-10-21 20:36:14 +0900

    Initial Commit

---
+++ YNMOSVersionCompat/YNMOSVersionCompat.m
@@ -0,0 +1,13 @@
+//
+//  YNMOSVersionCompat.m
+//  YNMOSVersionCompat
+//
+//  Created by yanamura on 2014/10/21.
+//  Copyright (c) 2014年 Yasuharu Yanamura. All rights reserved.
+//
+
+#import "YNMOSVersionCompat.h"
+
+@implementation YNMOSVersionCompat
+
+@end
